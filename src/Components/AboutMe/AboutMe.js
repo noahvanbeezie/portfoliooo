eb3d766ee33088b0b7118bd67e37c2f9ccb4388a
DogSniffers <DogSniffers@users.noreply.github.com>
2021-01-03 20:37:57 -0700

    added bootstrap css file, and fixed all component css

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,4 +1,8 @@
 import React, {useState,useEffect} from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import image1 from '../assets/img/71798196_10114345896654953_6945831496322121728_o.jpg'
+import image2 from '../assets/img/be39725049b1bf0adf6bba530b0baedc.jpg'
+import image3 from '../assets/img/retro-futuristic-synthwave-retrowave-styled-night-cityscape-with-sunset-background_148087-120.jpg'
 import './AboutMe.css'
 
 
@@ -13,6 +17,26 @@ function AboutMe(){
                         <p>I'm a junior level web-developer looking to learn new tech, and build on current knowledge.</p>
                     </div>
                     <div className='amImages'>
+                        <Carousel className='carousel'>
+                            <Carousel.Item interval={2000}
+                            className='carouselImage'>
+                                <img
+                                className='amImage'
+                                src={image1}/>
+                            </Carousel.Item>
+                            <Carousel.Item interval={2000}
+                            className='carouselImage'>
+                                <img
+                                className='amImage'
+                                src={image2}/>
+                            </Carousel.Item>
+                            <Carousel.Item interval={2000}
+                            className='carouselImage'>
+                                <img
+                                className='amImage'
+                                src={image3}/>
+                            </Carousel.Item>
+                        </Carousel>
                     </div>
                 </div>
             </div>
